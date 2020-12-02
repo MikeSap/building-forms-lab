4c654daefb8e76e6fb5da173b7ed32feb3b6a692
mike <mikesapienzaaudio@gmail.com>
2020-12-01 21:08:11 -0600

    functional but tests not passing

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
+
+
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <ul>
+        {this.props.bands.map(band => {
+         return <li>{band}</li>
+        })}
+        </ul>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = (state) => {
+  return {bands: state.bands}
+}
+
+const mapDispatchToProps = dispatch => ({
+  addBand: formData => dispatch({ type: 'ADD_BAND', band: formData })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+
