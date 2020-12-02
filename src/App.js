56d1a909a58a0db124c6a474576e638bd001c4f6
mike <mikesapienzaaudio@gmail.com>
2020-12-01 21:07:55 -0600

    commit tree efe6678dad71079e6692b4643a45339040ba3b7e

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import BandsContainer from './containers/BandsContainer';
+import manageBand from './reducers/manageBand'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+let store = createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <BandsContainer />
       </div>
+      </Provider>
     );
   }
 };
--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,11 +1,30 @@
-// Add BandInput component
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  state = {
+    text:""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+    [e.target.name]: e.target.value
+    })    
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addBand(this.state.text)
+    this.setState({text:""})
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} name="text" type="text" value={this.state.text}/>
+          <input type="submit"></input>
+        </form>
       </div>
     )
   }
